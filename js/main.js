cda25f27a0ac88e4f319fca857de4983d3934d91
gurworla <rlawogur0533@naver.com>
2023-08-30 15:04:04 +0900

    Start project

---
+++ js/main.js
@@ -0,0 +1,144 @@
+const searchEl = document.querySelector(".inner_submenu_search");
+const searchInputEl = searchEl.querySelector("input");
+
+searchEl.addEventListener("click", function () {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener("focus", function () {
+  searchEl.classList.add("focused");
+  searchInputEl.setAttribute("placeholder", "통합검색");
+});
+
+searchInputEl.addEventListener("blur", function () {
+  searchEl.classList.remove("focused");
+  searchInputEl.setAttribute("placeholder", "");
+});
+
+const badgeEl = document.querySelector("header .badges");
+const toTopEl = document.querySelector("#to-top");
+
+// _.throttle(함수,시간)
+// gsap.to(요소,지속시간,옵션)
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 500) {
+      //배지 숨기기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+      //버튼 보이기
+      gsap.to(toTopEl, 0.2, {
+        x: 0,
+      });
+    } else {
+      //배지 보이기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+      //버튼 숨기기
+      gsap.to(toTopEl, 0.2, {
+        x: 100,
+      });
+    }
+  }, 300)
+);
+
+toTopEl.addEventListener("click", function () {
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});
+
+new Swiper(".notice-line .swiper", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true, //반복재생
+});
+
+new Swiper(".promotion .swiper", {
+  slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true, //1번 슬라이드가 가운데
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
+
+new Swiper(".awards .swiper", {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: ".awards .swiper-prev",
+    nextEl: ".awards .swiper-next",
+  },
+});
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener("click", function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    promotionEl.classList.add("hide");
+  } else {
+    promotionEl.classList.remove("hide");
+  }
+});
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
+    repeat: -1,
+    yoyo: true, //역재생
+    ease: Power1.easeInOut,
+    delay: random(0, delay),
+  });
+}
+
+floatingObject(".floating1", 1, 15);
+floatingObject(".floating2", 0.5, 15);
+floatingObject(".floating3", 1.5, 20);
+
+const spyEls = document.querySelectorAll("section.scroll-spy");
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    triggerElement: spyEl, //보여짐 여부를 감시할 요소를 지정
+    triggerHook: 0.8,
+  })
+    .setClassToggle(spyEl, "show")
+    .addTo(new ScrollMagic.Controller());
+});
+
+const thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear();
---
+++ js/youtube.js
@@ -0,0 +1,23 @@
+var tag = document.createElement("script");
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+function onYouTubeIframeAPIReady() {
+  new YT.Player("player", {
+    videoId: "An6LvWQuj_8", //유튜브 ID
+    playerVars: {
+      autoplay: true,
+      loop: true,
+      playlist: "An6LvWQuj_8",
+    },
+    events: {
+      onReady: function (event) {
+        event.target.mute(); //음소거
+      },
+    },
+  });
+}
